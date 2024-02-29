8a82cbd28ba5228318486c9c9593c7d57a82f6bb
cleciopinheiro <clecio.pinheiro@outlook.com>
2024-02-29 10:03:30 -0300

    fix: Ajustes na section Hero

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -9,7 +9,7 @@ function Hero() {
 
     return (
         <section id="home" className="flex items-center justify-between bg-[var(--primary)] px-[10%] py-16 pb-24 sm:pt-28 sm:px-6 sm:justify-center sm:flex-col sm:gap-8 sm:pb-8 md:px-6">
-            <div className="flex flex-col items-center gap-8 md:items-start">
+            <div className="flex flex-col gap-8 md:items-start">
                 <h1 className="font-bold text-[var(--accent)] text-4xl sm:text-3xl sm:text-center md:text-2xl">LIMPEZA & HIGIENIZAÇÃO<br /> <span className="text-white">DE ESTOFADOS É A NOSSA ESPECIALIDADE</span></h1>
                 <p className="w-[70%] text-white sm:text-center md:w-full">Utilizando produtos e equipamentos <span className="font-bold">especializados</span>, promovemos uma <span className="font-bold">limpeza profunda</span>, deixando o estofamento <span className="font-bold">totalmente limpo, livre de bactérias, fungos e ácaros.</span></p>
                 <Button text="Solicitar orçamento" icon={<BsWhatsapp size={32}/>} onClick={handleWhatsapp} style="w-[50%] text-[18px] sm:w-full md:w-[80%]" />
